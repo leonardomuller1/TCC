e949be423275d1cfd78d8701b7193a951ca785ed
Leonardo Muller <leoanrdomuller78@gmail.com>
2024-07-24 13:40:38 -0300

    feat: link layout

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,13 +17,14 @@ const buttonVariants = cva(
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-gray-500 text-xs underline-offset-4 hover:underline',
+        link: 'text-gray-500 font-normal text-xs underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10',
+        link: 'p-0	'
       },
     },
     defaultVariants: {
