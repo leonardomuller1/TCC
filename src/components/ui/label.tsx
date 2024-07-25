d938e6efa7bb3c0300ea3816555a5a2eb69d7267
Leonardo Muller <leoanrdomuller78@gmail.com>
2024-07-25 09:25:03 -0300

    feat: add label component

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '/src/lib/utils';
 
 const labelVariants = cva(
-  'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70',
+  'text-sm font-semibold text-gray-700',
 );
 
 const Label = React.forwardRef<
