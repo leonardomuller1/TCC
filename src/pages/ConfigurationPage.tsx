b057a30d38f0b06fad3de3ca382e84e21fc56402
Leonardo Muller <leoanrdomuller78@gmail.com>
2024-07-25 15:16:58 -0300

    feat: add configuration page

--- src/pages/ConfigurationPage.tsx
+++ src/pages/ConfigurationPage.tsx
@@ -1,59 +1,129 @@
-import { useState } from 'react';
-import { useDropzone } from 'react-dropzone';
+import { useEffect, useState } from 'react';
+
+//components
 import CardPages from '@/components/dashboard/CardPagesComponent';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { useToast } from '@/components/ui/use-toast';
+import { Toaster } from '@/components/ui/toaster';
 
-interface ImageFile extends File {
-  preview: string;
-}
+//auxilares
+import useAuthStore from '@/stores/useAuthStore';
+import { supabase } from '@/supabaseClient';
 
 function ConfigurationPage() {
-  const [image, setImage] = useState<ImageFile | null>(null);
-  const { getRootProps, getInputProps } = useDropzone({
-    accept: { 'image/*': [] },
-    onDrop: acceptedFiles => {
-      const file = acceptedFiles[0] as unknown as ImageFile;
-      setImage(Object.assign(file, {
-        preview: URL.createObjectURL(file)
-      }));
+  const { user } = useAuthStore();
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (user) {
+      const fetchUserData = async () => {
+        const { data, error } = await supabase
+          .from('usuarios')
+          .select('email,nome,foto')
+          .eq('id', user.id)
+          .single();
+        if (data) {
+          setEmail(data.email);
+          setName(data.nome);
+        } else if (error) {
+          toast({
+            description: error.message,
+            className: 'bg-red-300',
+            duration: 4000,
+          });
+        }
+      };
+      fetchUserData();
     }
-  });
+  }, [user, toast]);
+
+  const handleUpdateProfile = async (e: { preventDefault: () => void; }) =>{
+    e.preventDefault();
+
+    const updates: { [key: string]: unknown } = {};
+
+    if(name){
+      updates.nome = name;
+    }
+
+    const {error:updateError} = await supabase.from('usuarios').update(updates).eq('id', user?.id)
+
+    if(updateError){
+      toast({
+        description: updateError.message,
+        className: "bg-red-300",
+        duration: 4000,
+      })
+      return;
+    }
+
+    if (password) {
+      const { error: passwordError } = await supabase.auth.updateUser({
+        password,
+      });
+
+      if (passwordError) {
+        if(passwordError.message == 'New password should be different from the old password.'){
+          toast({
+            description: 'A nova senha deve ser diferente da senha antiga.',
+            className: "bg-red-300",
+            duration: 4000,
+          });
+          return;
+        }
+        toast({
+          description: passwordError.message,
+          className: "bg-red-300",
+          duration: 4000,
+        });
+        return;
+      }
+    }
+
+    toast({
+      description: "Perfil atualizado com sucesso!",
+      className: "bg-green-300",
+      duration: 4000,
+    });
+  }
 
   return (
     <CardPages>
       <h1 className="text-gray-900 font-bold text-2xl">Configurações</h1>
-      <form className="gap-4 flex flex-col w-1/3">
+      <form className="gap-4 flex flex-col w-1/3" onSubmit={handleUpdateProfile}>
         <div className="gap-2 flex flex-col">
           <Label>Nome</Label>
-          <Input type="text" />
+          <Input
+            type="text"
+            value={name}
+            id="name"
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className="gap-2 flex flex-col">
           <Label>E-mail</Label>
-          <Input type="email" />
+          <Input type="email" value={email} id="email" readOnly />
         </div>
         <div className="gap-2 flex flex-col">
           <Label>Foto</Label>
-          <div {...getRootProps()} className="rounded-md border border-gray-300 p-4 cursor-pointer">
-            <input {...getInputProps()} />
-            {image ? (
-              <img src={image.preview} alt="Preview" className="w-32 h-32 object-cover" />
-            ) : (
-              <p className='text-sm text-gray-500'>Arraste e solte a imagem aqui, ou clique para selecionar uma imagem</p>
-            )}
-          </div>
-        </div>
-        <div className="gap-2 flex flex-col">
-          <Label>Senha atual</Label>
-          <Input type="password" />
         </div>
         <div className="gap-2 flex flex-col">
           <Label>Senha nova</Label>
-          <Input type="password" />
+          <Input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <Button>Alterar dados</Button>
+        <Button type='submit'>Alterar dados</Button>
       </form>
+      <Toaster />
     </CardPages>
   );
 }
