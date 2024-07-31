6fb1d95c77be5e55a75d89ce78d0462694127217
Leonardo Muller <leoanrdomuller78@gmail.com>
2024-07-31 12:38:07 -0300

    feat: add InputGroup Component

---
+++ src/components/InputGroupComponent.tsx
@@ -0,0 +1,37 @@
+//componentes
+
+import { Input } from './ui/input';
+import { Label } from './ui/label';
+
+//interface
+interface InputGroupProps {
+  title?: string;
+  subtitle?: string;
+  label: string;
+  placeholder: string;
+  value: string;
+  divider: boolean;
+}
+
+const InputGroup: React.FC<InputGroupProps> = ({
+  title,
+  subtitle,
+  label,
+  placeholder,
+  value,
+  divider,
+}) => {
+  return (
+    <div className="max-w-2xl flex flex-col gap-2">
+      <div className='pb-2'>
+        <h4 className="text-gray-900 text-lg font-semibold">{title}</h4>
+        <h5 className='text-gray-500 text-sm font-normal'>{subtitle}</h5>
+      </div>
+      <Label>{label}</Label>
+      <Input placeholder={placeholder} value={value} />
+      {divider && <div className="h-px w-full bg-gray-200"></div>}
+    </div>
+  );
+};
+
+export default InputGroup;
