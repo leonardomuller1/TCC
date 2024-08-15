f196212e7b4cd8d099f569422cc5bffc053258aa
Leonardo Muller <leoanrdomuller78@gmail.com>
2024-08-15 09:57:48 -0300

    fix: bg bug in the height in the CardPagesComponent

--- src/components/dashboard/CardPagesComponent.tsx
+++ src/components/dashboard/CardPagesComponent.tsx
@@ -7,7 +7,7 @@ interface CardProps {
 
 const CardPages: React.FC<CardProps> = ({ children, className = '' }) => {
   return (
-    <div className="bg-gray-50 h-screen">
+    <div className="bg-gray-50 min-h-screen h-full">
       <HeaderTopDashboard />
       <div
         className={`mt-8 mx-32 p-6 flex flex-col gap-4 border rounded-lg border-gray-200 ${className}`}
